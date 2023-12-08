79268b0c966b8fe8ba1c4b752bd304c3843971b6
serik <bukenoff.s@gmail.com>
2023-12-08 21:53:57 +0700

    :sparkles: Show panel

---
+++ src/components/ProgressPanel/ProgressPanel.tsx
@@ -0,0 +1,17 @@
+import { Accessor, Component } from "solid-js";
+import styles from "./ProgressPanel.module.css";
+
+type ProgressPanelProps = {
+  restart: () => void;
+  minesLeft: Accessor<number>;
+};
+
+export const ProgressPanel: Component<ProgressPanelProps> = (props) => {
+  return (
+    <div class={styles["container"]}>
+      <span>{props.minesLeft()}</span>
+      <button onClick={props.restart}>r</button>
+      <span>000</span>
+    </div>
+  );
+};
