d9fbc2ee28d5bd2143cd8568ded1de1b51b746be
serik <bukenoff.s@gmail.com>
2024-11-10 11:12:29 +0700

    Abandon the idea to move algorithm to rust

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,8 @@
-import { createSignal } from "solid-js";
-import logo from "./assets/logo.svg";
-import { invoke } from "@tauri-apps/api/tauri";
 import "./App.css";
 import { BoardProvider } from "./contexts";
 import { Board } from "./components";
 
 function App() {
-  const [greetMsg, setGreetMsg] = createSignal("");
-  const [name, setName] = createSignal("");
-
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name: name() }));
-  }
-
   return (
     <div class="container">
       <BoardProvider>
